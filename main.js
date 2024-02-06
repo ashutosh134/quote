18e4de6d90620df79581cedb7ee22a1ecfa4d45a
ashutosh134 <vermaashutosh134@gmail.com>
2024-02-06 12:23:30 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+
+
+let h1 = document.querySelector('h1');
+let h2 = document.querySelector('h2');
+
+let button = document.querySelector('button')
+
+
+
+const getQuote = async () => {
+    
+   const response = await fetch('https://quotable.io/random')
+
+   const data = await response.json()
+
+   h1.innerText = data.content;
+
+   h2.innerText = data.author;
+
+   console.log(data);
+}
+
+
+button.addEventListener('click',getQuote)
+
+setInterval(() => {
+    getQuote()
+}, 10000);
